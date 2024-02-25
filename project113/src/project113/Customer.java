19fe2cd7a7ebd13173a074aeee3d2891913b0aeb
lama <157112054+thisislama@users.noreply.github.com>
2024-02-25 20:23:28 +0300

    clean

--- project113/src/project113/Customer.java
+++ project113/src/project113/Customer.java
@@ -1,45 +1,41 @@
 package project113;
 import java.util.*;
 Static Scanner in = new Scanner(System.in);
-public class Customer {// by lama 
+public class Customer { 
 	private int customerID ;
 	private String name ;
 	private String DOB ;
 	 Account[] accounts;
 	 private int numOfAccounts ;
 	
-<<<<<<< HEAD
-	Customer (int cID , String name , String dob ){
-=======
-	Customer (String cID , String name , String dob , int numAcc){
->>>>>>> fbf3fcc3edc36cefd4a6d123473badee16956f7b
+	Customer (int cID , String name , String dob , int numAcc){
 		customerID = cID ;
 		this.name = name;
 		DOB = dob;
 		accounts = new Account[numAcc];	
 		numOfAccounts=0;
 	}
+	
 
+<<<<<<< HEAD
+=======
 <<<<<<< HEAD
 	}*//add
 	public int getCustomerID(){
 		return customerID;
 	}
 =======
+>>>>>>> ad66fb498617512155bb63d2b03874706ba45062
 	public void addAccount(Account account) {
 		 if(numOfAccounts<accounts.length)//
 		 accounts[numOfAccounts++]=account;//add this
 		 else
 		     System.out.println("Maximum account limit reach");
 		 }
-		 
-   
-    
-	public Account selectAccount() {
+
+public Account selectAccount() {
 		if (accounts != null && accounts.length > 0){
 			return list[0];
->>>>>>> fbf3fcc3edc36cefd4a6d123473badee16956f7b
-
 			System.out.println("is that your selected account? enter y or n");
 		 char opt = in.nextChar();
 		 while (opt == 'n'){
@@ -55,9 +51,12 @@ public class Customer {// by lama
 		System.out.println("You don't have accounts to select.");
 		return null;
 		}
+}// error should return Account obj :)
+	
+	public int getCustomerID(){
+		return customerID;
 	}
 
-	//public
 
 	public String toString() {
 	return "Customer ID= "+customerID+"\nName= "+name+"\nDate of Birth= "+DOB ;
