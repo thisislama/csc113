88bf52d4b912d84c7942c0df86fecc5be1621f1d
lama <lamaAlbugami@outlook.com>
2024-02-26 15:49:35 +0300

    main class added

---
+++ project113/src/project113/main.java
@@ -0,0 +1,10 @@
+package project113;
+import java.util.*;
+public class main {
+static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
