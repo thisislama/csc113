cd539b543eaafba48cd8dec5756d6b7fea9a96d3
aljawharah44 <161138602+aljawharah44@users.noreply.github.com>
2024-02-29 21:52:20 +0300

    added

--- project113/src/project113/ATMsystem.java
+++ project113/src/project113/ATMsystem.java
@@ -25,6 +25,22 @@ ATM atm=new ATM("QuickTeller ATM" , "Located in Riyadh" , "RYD001", 50000);
             case 2:
                 System.out.println("You chose to sign up.");
 //add customer obj
+do{
+            System.out.println("Enter your customer ID (between 10000000 and 99999999)");
+            int cID=in.nextInt();
+            
+            if(cID>=10000000 && cID<=99999999)
+            System.out.println("Invalid customer ID, pleas try again");
+           }while(cID>=10000000 && cID<=99999999)
+           
+            System.out.println("Enter your name");
+            in.next();
+            String name=in.nextLine();
+            System.out.println("Enter your date of birth");
+            in.next();
+            String dob=in.nextLine();
+            Customer customer1=new Customer (cID , name , dob);
+
                 break;
             default:
                 System.out.println("Invalid choice. Please enter 1 for log in or 2 for sign up.");
