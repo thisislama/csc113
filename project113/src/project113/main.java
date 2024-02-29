b1eb86fe403c0d6e9f6491f6f3fa027f3c318021
aljawharah44 <161138602+aljawharah44@users.noreply.github.com>
2024-02-29 13:46:40 +0300

    add main

--- project113/src/project113/main.java
+++ project113/src/project113/main.java
@@ -4,7 +4,25 @@ public class main {
 static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+// Ask the user if they want to log in or sign up
+        System.out.println("Welcome to the ATM System!");
+        System.out.println("1. Log in");
+        System.out.println("2. Sign up");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
 
+        switch (choice) {
+            case 1:
+                System.out.println("You chose to log in.");
+
+                break;
+            case 2:
+                System.out.println("You chose to sign up.");
+				
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter 1 for log in or 2 for sign up.");
+        }
 	}
 
 }
